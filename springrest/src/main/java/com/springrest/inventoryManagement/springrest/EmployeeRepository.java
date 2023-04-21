d71c858061995310c38137ef5a6202ea9b94285e
dogga.durga <dogga.durga@C1-716HYL3-L.ibtechnology.com>
2023-04-21 14:53:03 +0530

    small changes

--- springrest/src/main/java/com/springrest/inventoryManagement/springrest/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.inventoryManagement.springrest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.inventoryManagement.springrest.entities.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer > {
-
-}
